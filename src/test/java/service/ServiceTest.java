11decdf96014dc128439f83fd1cce661a8ccfcb6
Duca Cosmin <cosminduka@yahoo.com>
2024-03-27 15:23:29 +0200

    Added new test

--- src/test/java/service/ServiceTest.java
+++ src/test/java/service/ServiceTest.java
@@ -37,4 +37,39 @@ public class ServiceTest {
         assert service.saveStudent(id, n, 938) == 1;
 
     }
+    @Test
+    public void saveStudentName() {
+
+        Validator<Student> studentValidator = new StudentValidator();
+        Validator<Tema> temaValidator = new TemaValidator();
+        Validator<Nota> notaValidator = new NotaValidator();
+        StudentXMLRepository fileRepository1 = new StudentXMLRepository(studentValidator, "studenti.xml");
+        TemaXMLRepository fileRepository2 = new TemaXMLRepository(temaValidator, "teme.xml");
+        NotaXMLRepository fileRepository3 = new NotaXMLRepository(notaValidator, "note.xml");
+
+        Service service = new Service(fileRepository1, fileRepository2, fileRepository3);
+        String id = "1";
+        int gr = 400;
+
+        assert service.saveStudent(id, "", gr) == 1;
+        assert service.saveStudent(id, "Abc", gr) == 0;
+
+    }
+    @Test
+    public void saveStudentId() {
+
+        Validator<Student> studentValidator = new StudentValidator();
+        Validator<Tema> temaValidator = new TemaValidator();
+        Validator<Nota> notaValidator = new NotaValidator();
+        StudentXMLRepository fileRepository1 = new StudentXMLRepository(studentValidator, "studenti.xml");
+        TemaXMLRepository fileRepository2 = new TemaXMLRepository(temaValidator, "teme.xml");
+        NotaXMLRepository fileRepository3 = new NotaXMLRepository(notaValidator, "note.xml");
+
+        Service service = new Service(fileRepository1, fileRepository2, fileRepository3);
+        String n = "abc";
+        int gr = 400;
+
+        assert service.saveStudent("", n, gr) == 1;
+        assert service.saveStudent("id", n, gr) == 0;
+    }
 }
